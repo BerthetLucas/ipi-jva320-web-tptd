d71a0b48fd8d53301722881dd00fdfef989b719d
Berthet <lu.berthet@gmail.com>
2025-02-05 16:34:12 +0100

    Message en page d accueil

--- src/main/java/com/ipi/jva320/controller/HomeController.java
+++ src/main/java/com/ipi/jva320/controller/HomeController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-@GetMapping(value = "/")
+    @GetMapping(value = "/")
     public String home() {
-    return "home";
-}
+        return "home";
+    }
 }
